a189d059dc4ba3e34e37403d8260ab141429bad1
Bablu Verma <bablu.4tuners@gmail.com>
2025-12-08 17:00:11 +0530

    change backend url

--- src/Components/Helper/axiosinstance.js
+++ src/Components/Helper/axiosinstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // export const BASE_URL = "http://localhost:9090";
-export const BASE_URL = "https://backend.gaurastra.com";
-// export const BASE_URL = "https://offlinedev.gaurastra.com";
+export const BASE_URL = "https://testbackend.gaurastra.com";
+
 
 const axiosInstance = axios.create();
 
