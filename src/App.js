5bf2eaee7f2297ee2012901d48f40d627f06150b
Abdul Ahad <abdulahadansari8100@gmail.com>
2025-12-11 16:24:36 +0530

    add login functionality

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import store from "./Redux/Store";
-import NewProduct from "./product/newproduct";
-import ProductTable from "./product/product";
-import OrderList from "./Order/OrderList";
-import CategoryList from "./Category/CategoryList";
-import SubCategoryList from "./SubCategory/SubCategoryList";
-import UsersList from "./User/UsersList";
-import UserDetails from "./User/UserDetails";
-import BlogList from "./Blogs/BlogList";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
 import BlogDescription from "./Blogs/BlogDescription";
+import BlogList from "./Blogs/BlogList";
 import CreateBlog from "./Blogs/CreateBlog";
 import EditBlog from "./Blogs/EditBlog";
+import CategoryList from "./Category/CategoryList";
+import HomeComponent from "./Components/home";
 import LandingEditor from "./Components/Landing_Page/LandingEditor";
 import LPUploadsHistory from "./Components/Landing_Page/LPUploadsHistory";
-import CouponsList from "./Coupons/CouponsList";
+import LeadsList from "./Components/LeadsList";
+import Navbar from "./Components/Navbar";
 import OfferBannerForm from "./Components/OfferBannerForm";
 import OfferBannerHistory from "./Components/OfferBannerHistory";
-import DownloadQR from "./product/DownloadQR";
-import LeadsList from "./Components/LeadsList";
-import AddProduct from "./offline-admin/pages/AddProduct";
-import OfflineProductTable from "./offline-admin/pages/OfflineProductTable";
-import CreateWorkerAccount from "./offline-admin/components/CreateAccount";
-import WorkerList from "./offline-admin/components/WorkerList";
-import HomeComponent from "./Components/home";
-import Navbar from "./Components/Navbar";
-import "./App.css";
-import Sidebar from "./Components/Sidebar/sidebar";
 import OfflineLayout from "./Components/OfflineLayout";
 import OnlineLayout from "./Components/OnlineLayout";
+import Sidebar from "./Components/Sidebar/sidebar";
+import CouponsList from "./Coupons/CouponsList";
+import CreateWorkerAccount from "./offline-admin/components/CreateAccount";
 import Dashboard from "./offline-admin/components/Dashboard";
+import WorkerList from "./offline-admin/components/WorkerList";
+import AddProduct from "./offline-admin/pages/AddProduct";
+import OfflineProductTable from "./offline-admin/pages/OfflineProductTable";
+import OrderList from "./Order/OrderList";
+import DownloadQR from "./product/DownloadQR";
+import NewProduct from "./product/newproduct";
+import ProductTable from "./product/product";
+import store from "./Redux/Store";
+import SubCategoryList from "./SubCategory/SubCategoryList";
+import UserDetails from "./User/UserDetails";
+import UsersList from "./User/UsersList";
+import { Toaster } from "react-hot-toast";
+import Login from "./auth/Login";
 
 function App() {
   return (
     <Provider store={store}>
+      <Toaster />
       <BrowserRouter>
+
         <Navbar />
         <div className="page-wrapper">
           <Routes>
+            <Route path="/" element={<Login />} />
             {/* Blog Routes */}
-              <Route path="/" element={<Navigate to="/OnlineAdmin" replace />} />
+            <Route path="/online-admin" element={<Navigate to="/OnlineAdmin" replace />} />
             <Route path="/blogs" element={<BlogList />}></Route>
             <Route path="/blog/description" element={<OnlineLayout><BlogDescription /></OnlineLayout>}></Route>
             <Route path="/blog/edit/:id" element={<EditBlog />}></Route>
